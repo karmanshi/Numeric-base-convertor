6bd94f93d6906b3d4cc5204be47a87970273a9b9
Karmanshi Shrivastava <80811897+karmanshi@users.noreply.github.com>
2024-08-18 12:12:15 +0000

    Numeric-base-convertor

---
+++ numeric_base_convertor/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Convertor from './Component/Convertor';
+
+function App() {
+  return (
+    <Convertor/>
+ 
+  );
+}
+
+export default App;
